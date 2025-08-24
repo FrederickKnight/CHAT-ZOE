26834c2453eadda9d8cb884ae83e39c1957958fb
Frederick Knight <infisefir@gmail.com>
2025-08-23 23:26:49 -0600

    initial: Chat Web

---
+++ src/components/chat.tsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
+
+import Cookies from "js-cookie";
+
+import type { User } from "@lib/cookie_session";
+
+let socket: Socket;
+
+interface Message{
+    user:User;
+    msg:String;
+}
+
+export default function ChatTest(){
+
+    const [user,setUser] = useState<User | null>(null)
+    const [messages,setMessages] = useState<Message[]>([])
+    const [message,setMessage] = useState("")
+    const room = "room_1"
+    const URL_PATH = "https://api-chat-teez.onrender.com"
+
+    useEffect(() => {
+        const userJson =  Cookies.get("user")
+        if (userJson) setUser(JSON.parse(userJson))
+    },[])
+
+    useEffect(() => {
+        console.log("iniciado")
+        if (!user) return;
+
+        socket = io(URL_PATH,{ transports: ["websocket"] });
+
+        socket.on("connect", () => {
+            socket.emit("join", { user: user, room });
+        });
+
+        socket.on("message",(data) => {
+            setMessages(
+                prev => [...prev,{user:data.user, msg:data.msg}]
+            );
+        })
+
+        return () =>{
+            socket.disconnect()
+        };
+
+    },[user])
+
+    const sendMessage = () => {
+        if (message.trim()) {
+            socket.emit("send_room_message", { room: room, msg: message, user });
+            setMessage("");
+        }
+    };
+
+    return (
+        <div>
+            Bienvenido {user?.username}
+            <input value={message} onChange={e => setMessage(e.target.value)} placeholder="Mensaje"/>
+            <button onClick={sendMessage}>Enviar</button>
+            <div>
+                {messages.map(
+                    (m,u) => 
+                        <div key={u}>
+                            {m.msg}
+                        </div>
+                )}
+            </div>
+        </div>
+    );
+}
---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import {
+	validateSessionToken,
+	setSessionTokenCookie,
+	deleteSessionTokenCookie
+} from "@lib/cookie_session";
+
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+	const token = context.cookies.get("session")?.value ?? null;
+	if (token === null) {
+		context.locals.user = null;
+		context.locals.session = null;
+		return next();
+	}
+	const { session, user } = await validateSessionToken(token);
+	console.log(session,user)
+	if (session !== null) {
+		setSessionTokenCookie(context, token, session.expiresAt);
+	} else {
+		deleteSessionTokenCookie(context);
+	}
+
+	context.locals.session = session;
+	context.locals.user = user;
+
+	if (user !== null){
+		context.cookies.set(
+			"user", JSON.stringify(user),
+			{
+				path : "/",
+				httpOnly: false,
+				expires : session.expiresAt,
+				sameSite: "lax"
+			}
+		)
+	}
+
+	return next();
+});
